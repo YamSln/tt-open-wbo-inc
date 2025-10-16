3ac8be64769d9085d608d6b12618d3e1cad579a7
Yam Slonimski <yamslonimski@campus.technion.ac.il>
2025-10-16 12:42:24 +0300

    removed seed in weighted flow

--- code/algorithms/Alg_LinearSU_Clustering.cc
+++ code/algorithms/Alg_LinearSU_Clustering.cc
@@ -203,8 +203,6 @@ uint64_t LinearSUClustering::computeOriginalCost(vec<lbool> &currentModel,
   |________________________________________________________________________________________________@*/
 void LinearSUClustering::bmoSearch(){
 
-  std::srand ( 1971603567 );
-
   if (orderWeights.size() == 0)
 	{
 		// No soft clauses
